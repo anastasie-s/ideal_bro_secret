a9aa77c42825ea99d4337dda05d4ce9216516b29
AnastasieS <anastasies00@gmail.com>
2026-07-07 10:53:46 +0100

    Create admin.js

---
+++ admin.js
@@ -0,0 +1,75 @@
+const SHEETS_URL = "ТВОЙ_WEB_APP_URL_СЮДА";
+
+let players = [];
+let groupA = [];
+let groupB = [];
+
+const $ = (id) => document.getElementById(id);
+
+function jsonp(action) {
+  return new Promise((resolve, reject) => {
+    const callbackName = "idealbroAdmin_" + Date.now();
+
+    window[callbackName] = (response) => {
+      delete window[callbackName];
+      script.remove();
+      resolve(response);
+    };
+
+    const script = document.createElement("script");
+    script.src =
+      SHEETS_URL +
+      "?action=" + encodeURIComponent(action) +
+      "&callback=" + callbackName;
+
+    script.onerror = reject;
+    document.body.appendChild(script);
+  });
+}
+
+function renderList(id, items) {
+  $(id).innerHTML = items
+    .map((p) => `<div class="admin-item"><b>${p.name}</b></div>`)
+    .join("");
+}
+
+$("loadBtn").onclick = async () => {
+  $("playersList").innerHTML = "Загружаю...";
+
+  const response = await jsonp("getResults");
+
+  players = response.players || [];
+
+  renderList("playersList", players);
+};
+
+$("splitBtn").onclick = () => {
+  const shuffled = [...players].sort(() => Math.random() - 0.5);
+
+  groupA = shuffled.filter((_, i) => i % 2 === 0);
+  groupB = shuffled.filter((_, i) => i % 2 === 1);
+
+  renderList("groupA", groupA);
+  renderList("groupB", groupB);
+};
+
+$("pairBtn").onclick = () => {
+  const a = [...groupA];
+  const b = [...groupB];
+
+  const pairs = [];
+
+  while (a.length && b.length) {
+    const p1 = a.shift();
+    const p2 = b.shift();
+
+    pairs.push([p1, p2]);
+  }
+
+  $("pairsList").innerHTML = pairs
+    .map(
+      ([p1, p2]) =>
+        `<div class="admin-item pair"><b>${p1.name}</b><span>+</span><b>${p2.name}</b></div>`
+    )
+    .join("");
+};
